b4b842bfd0ae74be67499dc1be75f06290ee9a85
2021-5081\user <tes@test.com>
2026-03-31 16:45:50 +0900

    stage.

--- edit_image.py
+++ edit_image.py
@@ -3,6 +3,7 @@ from io import BytesIO
 import cv2
 import numpy as np
 
+<<<<<<< HEAD
 def add_images(text_img,bg_img):
     text_img, bg_img = list(map(lambda img:np.array(img),[text_img,bg_img]))
     ## 마스크 생성
@@ -13,7 +14,10 @@ def add_images(text_img,bg_img):
     src, dst = text_img, bg_img
     img_result = cv2.copyTo(src, mask, dst)
     img_result = cv2.cvtColor(img_result, cv2.COLOR_BGR2RGB)
+=======
+def add_images(img1,img2):
+    img1,img2=tuple(map(lambda img:np.array(img),[img1,img2]))
+    img_result=cv2.add(img1,img2)
+>>>>>>> parent of 2cff95d ([feat] develop module to add two images)
     img_bytes = cv2.imencode('.jpg', img_result)[1].tobytes()
     return img_bytes
-
-    
